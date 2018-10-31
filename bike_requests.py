c54fc334a66ca20af770301b773c78416f9ea57f
Katelyn Walker <walker.katelyn@gmail.com>
2018-10-31 10:26:47 -0700

    improving engineering

--- bike_requests.py
+++ bike_requests.py
@@ -5,7 +5,9 @@ import pandas as pd
 def single_query(link):
     '''
     Input: internet link to request json data from (string)
-    Output: Pandas df with columns of data for each bike, also a column for datetime of current request
+    Returns:
+        Pandas df with columns of data for each bike, also a column for datetime of current request
+        time before next update of API
     '''
     response = requests.get(link)
     if response.status_code != 200:
@@ -17,22 +19,44 @@ def single_query(link):
         # convert the bikeshare data into a pandas df, include a column for the datetime of the current call
         bike_data = pd.DataFrame(contents['data']['bikes'])
         bike_data['datetime'] = contents['last_updated']
-        return bike_data
+        bike_data['update_time'] = contents['ttl']
+        return bike_data, contents['ttl']
+
     
-link = 'https://sc.jumpbikes.com/opendata/free_bike_status.json'  ## enter bikeshare data link here
+def get_and_write_data(link, filename)
+    '''
+    Gets bikeshare data from link and appends it to a csv in filename.
+    
+    Inputs:
+        link: internet link to request json data from (string)
+        filename: file to append the csv data to (string)
+    
+    Returns: 
+        sleep_time: time to wait between requests (int)
+        Also writes data to a csv
+    '''
 
-# get the bikeshare data from the internet and write to a blank file in csv format
+        bike_data, sleep_time = single_query(link)
+        with open(filename, mode='a') as f:
+            f.write(bike_data.to_csv(header=False))
+        return sleep_time
 
-bike_data = single_query(link)
-with open('sac-bike-data.csv', mode='w+') as f:
-    f.write(bike_data.to_csv())
+    
+if __name__ == '__main__':
+    link = 'https://sc.jumpbikes.com/opendata/free_bike_status.json'
+    filename = 'sac-bike-data.csv'
 
-# once a minute, get the bikeshare data from the internet and continue appending in csv format to same file as above
-
-while True:
-#for k in [1,2,3,4]:
-    time.sleep(60)
+    # first query: get the bikeshare data from the internet and write to a blank file in csv format
 
     bike_data = single_query(link)
-    with open('sac-bike-data.csv', mode='a') as f:
-        f.write(bike_data.to_csv(header=False))
+    with open(filename, mode='w+') as f:
+        f.write(bike_data.to_csv())
+
+    # run an infinite loop to get continued data from API
+
+    while True:
+            sleep_time = get_and_write_data
+            time.sleep(sleep_time)
+
+
+
