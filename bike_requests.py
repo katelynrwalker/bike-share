a15cb6a06a6edb2f5808e7b09b2b99265ce56e57
Katelyn Walker <walker.katelyn@gmail.com>
2018-11-15 15:21:55 -0800

    add try-except to API requester

--- bike_requests.py
+++ bike_requests.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import pandas as pd
+import datetime
 
 def single_query(link):
     '''
@@ -15,7 +16,7 @@ def single_query(link):
         print('WARNING', response.status_code)
     else:
         contents = response.json()
-        
+
         # convert the bikeshare data into a pandas df, include a column for the datetime of the current call
         if 'station' in link:
             bike_data = pd.DataFrame(contents['data']['stations'])
@@ -25,16 +26,16 @@ def single_query(link):
         bike_data['update_time'] = contents['ttl']
         return bike_data
 
-    
+
 def get_and_write_data(link, filename):
     '''
     Gets bikeshare data from link and appends it to a csv in filename.
-    
+
     Inputs:
         link: internet link to request json data from (string)
         filename: file to append the csv data to (string)
-    
-    Returns: 
+
+    Returns:
         None (function writes data to a csv)
     '''
 
@@ -42,7 +43,7 @@ def get_and_write_data(link, filename):
     with open(filename, mode='a') as f:
         f.write(bike_data.to_csv(header=False))
 
-    
+
 if __name__ == '__main__':
     links = [
         'https://sc.jumpbikes.com/opendata/free_bike_status.json',
@@ -52,16 +53,16 @@ if __name__ == '__main__':
         'https://sf.jumpbikes.com/opendata/station_status.json',
         'https://sac.jumpbikes.com/opendata/station_status.json'
         ]
-    
+
     filenames = [
-        'sc-bike-data_1031.csv',
-        'sf-bike-data_1031.csv',
-        'sac-bike-data_1031.csv',
-        'sc-bike-stations_1031.csv',
-        'sf-bike-stations_1031.csv',
-        'sac-bike-stations_1031.csv'
+        'sc-bike-data.csv',
+        'sf-bike-data.csv',
+        'sac-bike-data.csv',
+        'sc-bike-stations.csv',
+        'sf-bike-stations.csv',
+        'sac-bike-stations.csv'
         ]
-    
+
 
     # first query: get the bikeshare data from the internet and write to a blank file in csv format
 
@@ -69,14 +70,14 @@ if __name__ == '__main__':
         bike_data = single_query(link)
         with open(filename, mode='w+') as f:
             f.write(bike_data.to_csv())
-            
+
     # run an infinite loop to get continued data from API
-    #Keyborad interrupt (ctrl+c to stop)
+    # Use keyboard interrupt (ctrl+c) to stop
 
     while True:
-        for link, filename in zip(links, filenames):
-            get_and_write_data(link, filename)
+        try:
+            for link, filename in zip(links, filenames):
+                get_and_write_data(link, filename)
+        except Exception as ex:
+            print ("{}: encountered problem: {}".format(datetime.datetime.now(), ex))
         time.sleep(60)
-
-
-
